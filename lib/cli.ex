0dfdbb638613e1065999062f1c89b1ecfa6edde8
bougueil <rmariana@gmail.com>
2024-05-11 12:41:38 +0200

    improve output of inspect

--- lib/cli.ex
+++ lib/cli.ex
@@ -55,12 +55,16 @@ defmodule Superls.CLI do
 
   defp main_args(["inspect"], store_name, passwd) do
     info = Api.inspect_store(store_name, passwd)
-    vols = Store.Reader.volume_path_from_digests(store_name, passwd) |> Enum.intersperse(", ")
+
+    vols =
+      ["" | Store.Reader.volume_path_from_digests(store_name, passwd)]
+      |> Enum.intersperse("\n  - ")
 
     IO.puts("""
+    Details for store `#{store_name}`:
+    100 most frequent tags: #{tags_to_string(info.most_frequent_100)}
     tags: #{info.num_tags},\nfiles: #{info.num_files},
-    100 most frequent tags: #{inspect(info.most_frequent_100, limit: :infinity, pretty: true)}
-    volumes: #{vols}
+    volumes:#{vols}
     """)
   end
 
@@ -69,13 +73,15 @@ defmodule Superls.CLI do
 
     IO.puts("""
     Usage: superls command [params] [-p -s my_store]
-      -p to password protect the store, the default store is `default`
+      -s specify a store name other than default one `default`
+      -p password protect the store
     Available commands are:
       archive:\n\t  superls archive /path/to/my/files\n\t  note: links prevent archiving `find /path/to/my/files -type l -ls`
       search:\n\t  superls search
       inspect:\n\t  superls inspect
     Stores:
       #{Enum.intersperse(Store.Reader.list_stores(), ", ")}
+
     Cache information:
       cache_path: #{path}
     """)
@@ -89,6 +95,12 @@ defmodule Superls.CLI do
       "** unknown command `#{Enum.intersperse(cmd, " ")}`\n" <>
         "type: `superls help` for available commands"
     )
+  end
+
+  defp tags_to_string(frequencies) do
+    Map.to_list(frequencies)
+    |> Enum.sort(&(elem(&1, 1) >= elem(&2, 1)))
+    |> Enum.map_join(", ", fn {t, num} -> "#{IO.ANSI.bright()}#{t}#{IO.ANSI.reset()} #{num}" end)
   end
 
   defp get_merged_index_from_store(store_name_or_path, password) do
