4fca5d85805b9c30d832a70a343c7c31ffb421b3
bougueil <rmariana@gmail.com>
2024-05-11 12:40:07 +0200

    fix SLS_SECRET in executable

--- config/config.exs
+++ config/config.exs
@@ -5,9 +5,4 @@ config :logger, :default_handler, level: :error
 
 config :logger, :default_formatter, format: "$time $message $metadata"
 
-config :superls,
-  secret_key_base:
-    System.get_env("SLS_SECRET") ||
-      "bqpBY/700YM3ns8e6fSAUtA4fx3/I+w/Xeoma6kn9xCS9XZc6KzWx54yl7XLHMIf"
-
 import_config "#{Mix.env()}.exs"
