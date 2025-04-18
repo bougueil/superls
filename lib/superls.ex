ad06386b51f573ee38bc29ade9a20d6a19b8a12f
bougueil <rmariana@gmail.com>
2025-04-18 12:14:17 +0200

    simplify, add types

--- lib/superls.ex
+++ lib/superls.ex
@@ -4,13 +4,23 @@ defmodule Superls do
              |> String.split("<!-- MDOC !-->")
              |> Enum.fetch!(1)
 
-  alias Superls.Prompt
+  @type store() :: String.t()
 
   @secret System.get_env("SLS_SECRET") ||
             "bqpBY/700YM3ns8e6fSAUtA4fx3/I+w/Xeoma6kn9xCS9XZc6KzWx54yl7XLHMIf"
 
   @doc false
-  def load_keys(file) do
+  if Mix.env() == :test do
+    def puts(_msg), do: :ok
+    def gets(_msg, default), do: default
+  else
+    def puts(msg), do: IO.puts(msg)
+    def gets(msg, _default), do: IO.gets(msg)
+  end
+
+  @doc false
+  # read tokens from private files 
+  def read_banned(file) do
     Stream.resource(
       fn -> File.open!(Application.app_dir(:superls, "priv/#{file}")) end,
       fn file ->
@@ -25,13 +35,13 @@ defmodule Superls do
     |> Map.from_keys(nil)
   end
 
+  @doc "interrupt flow with the stores_pathmessage `mg`"
+  @spec halt(String.t()) :: no_return()
   def halt(msg) do
     throw(msg)
   end
 
-  def cache_path,
-    do: Application.fetch_env!(:superls, :stores_path)
-
+  @doc "Returns `encrypted` from `term` with `passwd`"
   def encrypt(index, ""),
     do: index
 
@@ -40,6 +50,7 @@ defmodule Superls do
     |> Plug.Crypto.encrypt(password, index, max_age: :infinity)
   end
 
+  @doc "Returns `decrypted` from `term` with `passwd`"
   def decrypt(index, ""),
     do: {:ok, index}
 
@@ -48,43 +59,11 @@ defmodule Superls do
     |> Plug.Crypto.decrypt(password, index)
   end
 
-  def cache_store_path(store),
-    do: Path.expand("#{store}", cache_path())
-
-  def maybe_create_cache_path,
-    do: maybe_create_dir("", false)
-
-  def maybe_create_dir(store, confirm?) do
-    store_path = cache_store_path(store)
-    do_maybe_create_dir(File.exists?(store_path), store, store_path, confirm?)
-  end
-
-  defp do_maybe_create_dir(true, _store, store_path, _confirm?),
-    do: store_path
-
-  defp do_maybe_create_dir(false, store, store_path, confirm?) do
-    if Prompt.prompt("Confirm create a new store `#{store}` [N/y] ?", confirm?) do
-      :ok = File.mkdir_p!(store_path)
-    end
-
-    store_path
-  end
-
   @doc false
-  def pp_sz(size) when size > 10 * 1024 * 1024 * 1024 do
-    "#{div(size, 1024 * 1024 * 102) / 10} gB."
-  end
-
-  def pp_sz(size) when size > 10 * 1024 * 1024 do
-    "#{div(size, 1024 * 102) / 10} mB."
-  end
-
-  def pp_sz(size) when size > 10 * 1024 do
-    "#{div(size, 102) / 10} kB."
-  end
-
-  def pp_sz(size) do
-    "#{size} B."
+  @spec build_indexed_list([]) :: {length :: integer(), indexed_list :: []}
+  def build_indexed_list(list) do
+    len = length(list)
+    {len, do_build_indexed_list(list, {len, []})}
   end
 
   # https://stackoverflow.com/questions/22576658/convert-elixir-string-to-integer-or-float
@@ -100,10 +79,6 @@ defmodule Superls do
   defp _string_to_numeric({num, ".0"}, _val), do: num
   defp _string_to_numeric({_num, _str}, val), do: elem(Float.parse(val), 0)
 
-  @doc false
-  def build_indexed_list(list, len),
-    do: do_build_indexed_list(list, {len, []})
-
   defp do_build_indexed_list([], {_, list}), do: list
 
   defp do_build_indexed_list([f | rest], {cnt, acc}),
@@ -111,14 +86,10 @@ defmodule Superls do
 
   defmacro __using__(_) do
     quote do
+      @type store() :: String.t()
+
       @default_store Application.compile_env!(:superls, :default_store_name)
       defp default_store, do: @default_store
-
-      defp date_human_str(posix_time),
-        do: posix_time |> DateTime.from_unix!() |> DateTime.to_date() |> Date.to_string()
-
-      defp datetime_human_str(posix_time),
-        do: posix_time |> DateTime.from_unix!() |> DateTime.to_string()
     end
   end
 end
