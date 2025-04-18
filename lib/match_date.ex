51e5fa655bc4d9ed428e63c0b2b9b252d414b9aa
bougueil <rmariana@gmail.com>
2025-04-18 12:05:03 +0200

    format matching dates

--- lib/match_date.ex
+++ lib/match_date.ex
@@ -2,7 +2,10 @@ defmodule Superls.MatchDate do
   @day_seconds 3600 * 24
   @moduledoc false
   use Superls
+
   # list oldest / newest files according to mtime / atime
+
+  def size(result), do: length(result)
 
   @doc ~S"""
   returns files between date from a list of files according to cmd:
@@ -17,18 +20,13 @@ defmodule Superls.MatchDate do
     date_min = date_unix - ndays * @day_seconds
     date_max = date_unix + ndays * @day_seconds
 
-    IO.puts(
-      "search result files between #{from_unix!(date_min)} and #{from_unix!(date_max)} (#{ndays} days): "
-    )
-
-    Enum.reduce(files, [], fn e = {{f, _}, _}, acc ->
-      if field.(f) >= date_min and f.mtime <= date_max do
+    Enum.reduce(files, [], fn e = {_fp, fp_info, _vol}, acc ->
+      if field.(fp_info) >= date_min and fp_info.mtime <= date_max do
         [e | acc]
       else
         acc
       end
     end)
-    |> output_friendly(field)
   end
 
   @doc ~S"""
@@ -40,48 +38,41 @@ defmodule Superls.MatchDate do
   the default number of returned files is set by the config parameter `:num_files`
   """
   def search_oldness(files, cmd, nentries) do
-    {sorter, field} = sorter_field(cmd)
-
+    {sorter, _} = sorter_field(cmd)
     IO.puts("search newest/oldest (#{cmd}) files, max: (#{nentries} files): ")
 
-    Enum.sort(files, sorter)
+    files
+    |> Enum.sort(sorter)
     |> Enum.take(nentries)
-    |> output_friendly(field)
   end
 
   @doc false
-  def output_friendly(result, field) do
-    for {{f, path}, _} <- result do
-      (IO.ANSI.bright() <>
-         Path.basename(f.name) <>
-         IO.ANSI.reset() <>
-         "\t" <>
-         Superls.pp_sz(f.size) <>
-         "\t" <>
-         datetime_human_str(field.(f)) <>
-         "\t" <>
-         path <>
-         "/" <>
-         Path.dirname(f.name) <>
-         IO.ANSI.reset())
-      |> IO.puts()
+  def to_string(result, cmd) do
+    {_, field} = sorter_field(cmd)
+
+    for {fp, f_info, vol} <- result do
+      [
+        {field.(f_info), :date, []},
+        " ",
+        {f_info.size, :sizeb, []},
+        " ",
+        {Path.basename(fp), {:scr, 60}, [:bright]},
+        "  ",
+        {Path.join(vol, f_info.dir), :scr, []},
+        "\n"
+      ]
     end
+    |> StrFmt.to_string()
   end
 
   defp sorter_field(cmd) do
     case cmd do
-      "xo" -> {fn {{a, _}, _}, {{b, _}, _} -> a.mtime < b.mtime end, & &1.mtime}
-      "xn" -> {fn {{a, _}, _}, {{b, _}, _} -> a.mtime >= b.mtime end, & &1.mtime}
-      "ro" -> {fn {{a, _}, _}, {{b, _}, _} -> a.atime < b.atime end, & &1.atime}
-      "rn" -> {fn {{a, _}, _}, {{b, _}, _} -> a.atime >= b.atime end, & &1.atime}
+      "xo" -> {fn {_fpa, a, _vola}, {_fpb, b, _volb} -> a.mtime < b.mtime end, & &1.mtime}
+      "xn" -> {fn {_fpa, a, _vola}, {_fpb, b, _volb} -> a.mtime >= b.mtime end, & &1.mtime}
+      "ro" -> {fn {_fpa, a, _vola}, {_fpb, b, _volb} -> a.atime < b.atime end, & &1.atime}
+      "rn" -> {fn {_fpa, a, _vola}, {_fpb, b, _volb} -> a.atime >= b.atime end, & &1.atime}
       "xd" -> {nil, & &1.mtime}
       "rd" -> {nil, & &1.atime}
     end
-  end
-
-  defp from_unix!(posix) do
-    DateTime.from_unix!(posix)
-    |> DateTime.to_date()
-    |> Date.to_string()
   end
 end
