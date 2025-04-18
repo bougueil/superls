db0330995daf13993ad22547255bcbe1baf4c35a
bougueil <rmariana@gmail.com>
2025-04-18 12:17:22 +0200

    improve comments

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ config :superls,
   stores_path: :filename.basedir(:user_cache, "superls/stores"),
   default_store_name: "default",
   jaro_threshold: 0.8,
-  # size_threshold: in percent, 1 % means 2 sizes similar if less than 1% 
-  size_threshold: 0.05,
+  # size_threshold: in percent, a value of 1 means 2 sizes match if less than 1% 
+  size_threshold: 0.01,
   num_files_search_oldness: 1000,
   num_days_search_bydate: 30
