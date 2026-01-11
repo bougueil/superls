05f3b4d89c007c794fedbe1fe0701dd905b4fd19
bougueil <rmariana@gmail.com>
2026-01-11 16:17:33 +0100

    format_files

--- test/match_tag_test.exs
+++ test/match_tag_test.exs
@@ -115,7 +115,7 @@ defmodule Superls.MatchTagTest do
         HelperTest.get_merged_index(default_store())
         |> MergedIndex.search_bytag(tags_string)
         |> elem(0)
-        |> MatchTag.format()
+        |> MatchTag.format_files()
       end)
 
     assert String.contains?("#{result}", "1 entries")
